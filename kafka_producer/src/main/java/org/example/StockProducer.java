ff91bdff6ede619dff10f431d93ec3beefbc91a2
minhtuyenvp02 <119645277+minhtuyenvp02@users.noreply.github.com>
2023-12-19 01:44:28 +0700

    update

--- kafka_producer/src/main/java/org/example/StockProducer.java
+++ kafka_producer/src/main/java/org/example/StockProducer.java
@@ -16,6 +16,54 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+
+//import static com.sun.crypto.provider.AESCrypt.log;
+
+//package org.example;
+//
+//import org.apache.kafka.clients.consumer.internals.events.EventHandler;
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.Partitioner;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.StringSerializer;
+//import org.apache.kafka.connect.json.JsonSerializer
+//import org.apache.kafka.common.serialization.
+//import java.net.URI;
+//import java.util.Properties;
+//import java.util.concurrent.TimeUnit;
+//
+//public class StockProducer {
+//    public static void main(String[] args) throws InterruptedException {
+//
+//        // create Producer Properties
+//        Properties properties = new Properties();
+//        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+//        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+//        properties.setProperty(ProducerConfig.PARTITIONER_ADPATIVE_PARTITIONING_ENABLE_CONFIG, Partitioner.class.getName());
+//
+//
+//
+//        // create the Producer
+//        KafkaProducer<String, > producer = new KafkaProducer<>(properties);
+//
+//        String topic = "";
+//
+//        EventHandler eventHandler = new StockEnventHandler(producer, topic);
+//        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+//        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+//        EventSource eventSource = builder.build();
+//
+//
+//        // start the producer in another thread
+//        eventSource.start();
+//
+//        // we produce for 10 minutes and block the program until then
+//        TimeUnit.MINUTES.sleep(10);
+//
+//
+//    }
+//}
 public class StockProducer {
     private static Logger LOGGER = LoggerFactory.getLogger("StockProducer");
     private static final long PROGRESS_REPORTING_INTERVAL = 5;
